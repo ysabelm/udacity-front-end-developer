e0f0924ca55a9fc4ed862bfb7220d5d1999e7221
ytrofnom <ysabelmonfort@gmail.com>
2018-01-03 09:20:07 +0100

    Sets the href of the <a> in the first nav item to '#1'

--- jquery-sample-site/js/app.js
+++ jquery-sample-site/js/app.js
@@ -9,3 +9,10 @@ article2 = $('.featured');
 article3 = article2.next();
 article2.toggleClass('featured');
 article3.toggleClass('featured');
+
+var navList, firstItem, link;
+
+navList = $('.nav-list');
+firstItem = navList.children().first();
+link = firstItem.find('a');
+link.attr('href', '#1');
