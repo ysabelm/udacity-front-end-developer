7bb7ee8b7ef499d702520a677be9ee13c4bf1390
ytrofnom <ysabelmonfort@gmail.com>
2018-01-03 09:17:12 +0100

    Removes the class 'featured' from Article #2, adds it to Article #3

--- jquery-sample-site/js/app.js
+++ jquery-sample-site/js/app.js
@@ -1,4 +1,11 @@
 var featuredArticle;
 
-featuredArticle = $("li.featured").toggleClass("featured");
+featuredArticle = $('li.featured').toggleClass('featured');
 console.log(featuredArticle);
+
+var article2, article3;
+
+article2 = $('.featured');
+article3 = article2.next();
+article2.toggleClass('featured');
+article3.toggleClass('featured');
