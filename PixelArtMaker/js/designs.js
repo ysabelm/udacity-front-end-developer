42d1fa91acf9fd275520c168f2eec949fecb2e91
ytrofnom <ysabelmonfort@gmail.com>
2018-01-17 11:27:54 +0100

    Adds switch statement to liftOff exercice.

--- PixelArtMaker/js/designs.js
+++ PixelArtMaker/js/designs.js
@@ -1,10 +1,50 @@
 // Select color input
+let color = "";
 // Select size input
+let width = "";
+let height = "";
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+	
 
 // Your code goes here!
 
 }
+
+
+
+
+
+/*$(document).ready(function(){
+
+    $(".container").append("<div></div>");
+    $("div").css({
+        "display": "inline-block",
+        "width": "squareWidth px",
+        "height": "squareHeight px",
+        "background-color": "red"
+    });
+
+    });*/
+
+/*var rows = 8;
+var columns = 8;
+var $row = $("<div />", {
+    class: 'row'
+});
+var $square = $("<div />", {
+    class: 'square'
+});
+
+$(document).ready(function () {
+    //add columns to the the temp row object
+    for (var i = 0; i < columns; i++) {
+        $row.append($square.clone());
+    }
+    //clone the temp row object with the columns to the wrapper
+    for (var i = 0; i < rows; i++) {
+        $("#wrapper").append($row.clone());
+    }
+});*/
--- js_loops_exercices/liftOff.js
+++ js_loops_exercices/liftOff.js
@@ -18,3 +18,32 @@ while (x >= 0) {
 	}
 	x = x - 1;
 }
+
+/* Switch Statement
+
+var x = 60;
+while (x>=0) {
+	switch (x) {
+		case 50:
+			console.log("Orbiter transfers from ground to internal power");
+		break;
+		case 31:
+			console.log("Ground launch sequencer is go for auto sequence start");
+		break;
+		case 16:
+			console.log("Activate launch pad sound suppression system");
+		break;
+		case 10:
+			console.log("Activate main engine hydrogen burnoff system");
+		break;
+		case 6:
+			console.log("Main engine start");
+		break;
+		case 0:
+			console.log("Solid rocket booster ignition and liftoff!");
+		break;
+		default:
+			console.log("T-" + x + " seconds");		
+		}
+	x = x - 1;
+}*/
